729cd825b6d38067e17880ad7039d30836e18382
정찬원 <jcw0522@gachon.ac.kr>
2025-12-10 13:00:35 +0900

    feat: 애니메이션 적용

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import PageTitle from "./layouts/PageTitle";
 import Project1 from "../assets/project1.png";
 import { ExternalLink, Calendar, Users } from "lucide-react";
@@ -20,7 +20,7 @@ const TechStack = () => {
         subTitle="Problem-solving through creative development"
       />
 
-      <div className="w-full max-w-6xl p-10 bg-linear-to-br from-blue-600 to-blue-400 border border-[#272727] rounded-3xl">
+      <div className="w-full max-w-6xl p-10 bg-linear-to-br from-blue-600 to-blue-400 border border-[#272727] rounded-3xl overflow-hidden">
         <div className="flex items-center justify-between mb-2">
           <h1 className="text-3xl text-white font-medium">
             나만의 인사이트를 저장하는 북마크 플랫폼
@@ -47,15 +47,26 @@ const TechStack = () => {
             <Users size={18} /> <span>4명 (프론트엔드 개발)</span>
           </div>
         </div>
-        {isOpen && (
-          <div className="mt-10 -mb-10 -mx-10 p-10 rounded-b-2xl bg-linear-to-br from-[#271b38] to-[rgb(1,11,26)] border border-white/10">
-            <img
-              src={Project1}
-              alt="북마크 플랫폼"
-              className="w-150 h-80 rounded-lg"
-            />
-          </div>
-        )}
+
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: "auto", opacity: 1 }}
+              exit={{ height: 0, opacity: 0 }}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
+              className="overflow-hidden mt-10 -mb-10 -mx-10"
+            >
+              <div className="p-10 rounded-b-2xl bg-linear-to-br from-[#271b38] to-[rgb(1,11,26)] border border-white/10">
+                <img
+                  src={Project1}
+                  alt="북마크 플랫폼"
+                  className="w-150 h-80 rounded-lg"
+                />
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </motion.div>
   );
